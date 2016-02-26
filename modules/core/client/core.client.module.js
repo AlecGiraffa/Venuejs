45525fd5f53ee5adc3dcc038018bc5ca6f7a6e31
Shijie Ning <alec_n@valiantica.com>
2016-02-25 16:21:25 -0800

    deleted

--- modules/core/client/core.client.module.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-
-// Use Application configuration module to register a new module
-ApplicationConfiguration.registerModule('core');
-ApplicationConfiguration.registerModule('core.admin', ['core']);
-ApplicationConfiguration.registerModule('core.admin.routes', ['ui.router']);
